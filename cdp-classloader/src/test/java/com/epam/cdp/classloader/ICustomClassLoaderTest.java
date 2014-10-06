a8c115fafe5d6e4ff153beb4dde93280f28e9200
Ilya Kukushkin <ilya_kukushkin@epam.com>
2014-10-06 19:35:35 +0300

    Change test

--- cdp-classloader/src/test/java/com/epam/cdp/classloader/ICustomClassLoaderTest.java
+++ cdp-classloader/src/test/java/com/epam/cdp/classloader/ICustomClassLoaderTest.java
@@ -8,11 +8,13 @@ public class ICustomClassLoaderTest {
 
     @Test
     public void testFindClass() throws Exception {
-        ICustomClassLoader customClassLoader = new CacheClassLoader().setNext(new SystemClassLoader().setNext(new FileClassLoader()));
+        //ICustomClassLoader customClassLoader = new CacheClassLoader().setNext(new SystemClassLoader().setNext(new FileClassLoader()));
+        ICustomClassLoader customClassLoader = new CacheClassLoader().setNext(new JarClassLoader());
 
-        Class<?> clazz = customClassLoader.findClass("C:/Users/Ilya_Kukushkin/Desktop/classloader", "com.epam.cdp.TestClass");
+        Class<?> clazz = customClassLoader.findClass("C:/Users/Ilya_Kukushkin/Desktop/classloader/test.jar", "com.epam.cdp.TestClass");
         Object o = clazz.newInstance();
-        Method m = clazz.getMethod("f");
-        m.invoke(o);
+        Method m = clazz.getMethod("toString");
+        String s = (String) m.invoke(o);
+        System.out.println(s);
     }
 }
